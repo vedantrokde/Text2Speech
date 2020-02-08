9cdd437b5c74bdf037439c19344baa79ad43d1e4
vedantrokde <vedant.rokde2018@vitstudent.ac.in>
2020-02-08 15:29:47 +0530

    Initial Backend Build

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\lenovo(25)\l_p\open source\t2s\text-to-speech-python\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
